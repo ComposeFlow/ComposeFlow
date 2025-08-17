1ba1f2391dfd01286b6fc06298107b97830edc13
Takeshi Hagikura <shiketa1999@gmail.com>
2025-08-18 02:23:03 +0900

    Add desktop implementation for auth related code

---
+++ core/platform/src/commonMain/kotlin/io/composeflow/platform/Uri.kt
@@ -0,0 +1,11 @@
+package io.composeflow.platform
+
+/**
+ * Multiplatform URI representation
+ */
+expect class Uri(uri: String) {
+    val uri: String
+    override fun toString(): String
+}
+
+fun Uri.toJavaUri(): java.net.URI = java.net.URI(this.uri)
